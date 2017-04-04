2ba6f259c129c6747a03e2c5c54af5fec7c35243
mack0082 <mack.waters0803@gmail.com>
2017-04-03 21:46:36 -0400

    updated info and comments

--- Assignment4/StartForm.cs
+++ Assignment4/StartForm.cs
@@ -1,4 +1,11 @@
-﻿using System;
+﻿// App name	           : Dollar Computers
+// Author's name       : Mayank Bhardwaj
+// Student ID          : 200305245
+// App Creation Date   : 3/13/2017
+// App description     : The app will allow users to select a laptop they want to buy and 
+//                       gives the user all the specifications and total price. 
+
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
